ece63440bd71f0eff428194243a06ff98ec0b82c
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-24 15:40:46 +0200

    Feat: Add image in HTML with Vue data

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // Createapp Vue
-const { createApp } = Vue
+const { createApp } = Vue;
 
-// Messaggio nell'H1 usando i data
+// Messaggio nell'H1 usando i data e aggiunta IMG 
 createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        imgUrl: `https://www.wisnet.com/wp-content/uploads/2021/07/vue-js-logo-png-transparent-png.png`,
+        imgAlt: `Logo Vue`,
       }
     }
-  }).mount('#app')
+  }).mount('#app');
